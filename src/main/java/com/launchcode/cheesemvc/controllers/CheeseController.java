897758ebe9d2aa0ec258a4e0f0effc650a155270
pedroaff <pedroafaria91@gmail.com>
2019-10-28 16:15:10 -0300

    create store method and move list of cheese to a static variable in class. fixed the form method

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,6 +3,8 @@ package com.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -10,14 +12,10 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     @RequestMapping("")
     public String index(Model model) {
-
-        ArrayList<String> cheeses = new ArrayList<>();
-
-        cheeses.add("cheddar");
-        cheeses.add("parmesan");
-        cheeses.add("gorgonzola");
 
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
@@ -25,11 +23,19 @@ public class CheeseController {
         return "cheese/index";
     }
 
-    @RequestMapping("create")
+    @RequestMapping(value = "create", method = RequestMethod.GET)
     public String create(Model model) {
 
         model.addAttribute("title", "Create");
 
         return "cheese/create";
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String store(@RequestParam String name) {
+
+        cheeses.add(name);
+
+        return "redirect:";
     }
 }
