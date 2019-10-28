ae50f8127fcfa993448d1b7377d186b3764026d8
pedroaff <pedroafaria91@gmail.com>
2019-10-28 20:10:58 -0300

    create model class

---
+++ src/main/java/com/launchcode/cheesemvc/models/Cheese.java
@@ -0,0 +1,28 @@
+package com.launchcode.cheesemvc.models;
+
+public class Cheese {
+
+    private String name;
+    private String description;
+
+    public Cheese(String name, String description) {
+        this.setName(name);
+        this.setDescription(description);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
