71dcfa053c763654d75cb3c3535999de2cfff83b
pedroaff <pedroafaria91@gmail.com>
2019-10-28 14:21:47 -0300

    create and return array list to view using foreach

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
@@ -11,6 +13,13 @@ public class CheeseController {
     @RequestMapping("")
     public String index(Model model) {
 
+        ArrayList<String> cheeses = new ArrayList<>();
+
+        cheeses.add("cheddar");
+        cheeses.add("parmesan");
+        cheeses.add("gorgonzola");
+
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
 
         return "cheese/index";
