bb478d28bf3cfd771db4f7a3b71ea0bdff952a9e
pedroaff <pedroafaria91@gmail.com>
2019-10-28 14:14:01 -0300

    initial commit with controller and index view

---
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,18 @@
+package com.launchcode.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    @RequestMapping("")
+    public String index(Model model) {
+
+        model.addAttribute("title", "My Cheeses");
+
+        return "cheese/index";
+    }
+}
---
+++ src/test/java/com/launchcode/cheesemvc/CheeseMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.cheesemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
